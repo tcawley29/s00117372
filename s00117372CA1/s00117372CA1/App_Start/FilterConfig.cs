f304bf1af32634f21c595ae76b44e361272c0b74
tcawley29 <tommy.cawley@hotmail.com>
2013-11-12 05:08:25 +0000

    Connected To DB and Displaying Orders

---
+++ s00117372CA1/s00117372CA1/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace s00117372CA1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ s00117372CA1/s00117372CA1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="s00117372CA1.MvcApplication" Language="C#" %>
